9296dc045b7f26b1c0a51873615ff708b94082bd
The-Violinist <violinist.david@yahoo.com>
2021-07-30 22:09:16 +0000

    Completed 05 demo

--- 05loop.sh
+++ 05loop.sh
@@ -5,13 +5,45 @@
 # Date of latest revision:
 # Purpose:
 
-# Basic for loop
-names="Stan Kyle Cartman"
-for name in $names
+###FUNCTIONS###
+#Ask is the user would like to stop a process.
+ask_user (){
+echo Would you like to kill a process?
+read yes_no
+}
+
+#Ask is the user would like to stop a process.
+ask_again (){
+echo Would you like to kill another process?
+read yes_no_again
+}
+
+#Loop to stop processes
+kill_process (){
+while true
 do
-  echo $name
+    echo Please enter a PID number to end a process
+    read pid_num
+    kill $pid_num
+    ask_again
+    if [ $yes_no_again == no ]
+    then
+        exit
+    elif [ $yes_no_again == yes ]
+    then
+        ps -aux
+    fi
 done
+}
 
-echo "Script complete"
 
+#MAIN
+ps -aux
+ask_user
+if [ $yes_no == yes ]
+then
+    kill_process
+else
+    echo Ok, I will not end any processes
+fi
 # End
