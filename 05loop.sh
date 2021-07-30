e059a2e09942e922da1598f4c5db7b14e71d6864
The-Violinist <violinist.david@yahoo.com>
2021-07-30 17:29:47 +0000

    Class 05

---
+++ 05loop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script:
+# Author:
+# Date of latest revision:
+# Purpose:
+
+# Basic for loop
+names="Stan Kyle Cartman"
+for name in $names
+do
+  echo $name
+done
+
+echo "Script complete"
+
+# End
